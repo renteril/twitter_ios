7ba93a3997a5fe01c2a1300ab978617a52c098f3
renteril <renteril@oregonstate.edu>
2021-03-03 20:38:36 -0800

    Update: implementing Twitter login, logout, API response, and Tweet design.

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,53 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Luis Renteria on 3/2/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func onLogout(_ sender: Any) {
+        
+        TwitterAPICaller.client?.logout()
+        self.dismiss(animated: true, completion: nil)
+        
+        UserDefaults.standard.set(false, forKey: "userLoggedIn")
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCellTableViewCell
+        
+        cell.userNameLabel.text = "Some name"
+        cell.tweetContent.text = "Something"
+        
+        
+        return cell
+    }
+    
+    
+    
+    
+    
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+
+
+
+}
